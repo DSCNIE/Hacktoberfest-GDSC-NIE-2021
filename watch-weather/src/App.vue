c1df089acff5eed65d5d4dd9cd053b48b8f727b7
Apala Singh <61432616+ApalaS@users.noreply.github.com>
2021-10-18 14:11:56 +0530

    Add files via upload

---
+++ watch-weather/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <index />
+  </div>
+</template>
+
+<script>
+import index from "./components/index.vue";
+
+export default {
+  name: "App",
+  components: {
+    index,
+  },
+};
+</script>
+
+<style>
+#app {
+  padding: 0;
+  margin: 0;
+}
+</style>

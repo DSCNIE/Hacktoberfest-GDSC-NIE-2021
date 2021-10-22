3a792c0a2d9f3b99bcadf3d0b3297b28737a7ba4
Apala Singh <61432616+ApalaS@users.noreply.github.com>
2021-10-22 08:03:12 +0530

    Add files via upload

---
+++ basic-calculator/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <calci />
+  </div>
+</template>
+
+<script>
+import calci from "./components/calci.vue";
+
+export default {
+  name: "App",
+  components: {
+    calci,
+  },
+};
+</script>
+
+<style>
+
+</style>
